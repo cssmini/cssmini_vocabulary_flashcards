8d0cad6a5ce349b698d85814e558293242a6447f
cssmini <cssmini@foxmail.com>
2023-04-09 04:46:32 +0800

    init

---
+++ config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+const $config = {
+  debug: false, // debug
+  key: '1abcdef0123456789',
+  key1: '1abcdef0123456789',
+  key2: '1abcdef0123456789',
+  key3: '1abcdef0123456789',
+  key4: '1abcdef0123456789',
+  key5: '1abcdef0123456789',
+  urlMap: {
+    development: '/racaData',
+    production: '/racaData'
+  }
+}
